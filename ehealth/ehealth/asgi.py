ac2fa4ecc7dfa2fc49daff97ca9becceed72b3e0
salahar9 <salahar9@gmail.com>
2022-05-17 01:27:31 +0100

    bugs

--- ehealth/ehealth/asgi.py
+++ ehealth/ehealth/asgi.py
@@ -16,7 +16,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import notifications.routing
-import django_chatter.routing
 
 
 
