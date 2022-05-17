3e102173a9bfdda4d139f5819ec5f28a55c8c8a9
salahar9 <salahar9@gmail.com>
2022-05-17 02:31:31 +0100

    card id to string

--- ehealth/patient/models.py
+++ ehealth/patient/models.py
@@ -6,7 +6,8 @@ class Patient(models.Model):
 	class Gender(models.TextChoices):
 		Male = 'M'
 		Female = 'F'
-	card_id=models.CharField(max_length=30,blank=True,null=True)
+	card_id=models.IntegerField(blank=True,null=True)
+	card_id_str=models.CharField(max_length=30,blank=True,null=True)
 	permission_privacy=models.BooleanField(default=False)
 	a_mutuelle=models.BooleanField(default=False,null=True,blank=True)
 	immatriculation=models.IntegerField(null=True,blank=True)
