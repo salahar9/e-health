9d708637d81d5904e5a2a00eef23d303c49c0e67
salahar9 <salahar9@gmail.com>
2022-05-18 16:00:55 +0100

    fixing sorting ?

--- ehealth/chat/views.py
+++ ehealth/chat/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from chat.models import MessageModel
 def chat(request,pk):
 	return render(request,"chat/chats.html",{})
+@require_POST
+@csrf_exempt
+def send_message(request):
+	sender=request.POST["sender"]
+	to=request.POST["to"]
+	message=request.POST["message"]
+	msg=MessageModel(sender=sender,to=to,msg=body)
+	msg.save()
+
