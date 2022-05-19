a6040f2bf5f9a28752f0d349fbac2b5068afa45e
salahar9 <salahar9@gmail.com>
2022-05-19 08:49:27 +0100

    fixing chat models indentation

--- ehealth/chat/models.py
+++ ehealth/chat/models.py
@@ -8,16 +8,10 @@ from channels.layers import get_channel_layer
 	
 
 class MessageModel(Model):
-    """
-    This class represents a chat message. It has a owner (user), timestamp and
-    the message body.
-
-    """
     sender= ForeignKey(Person, on_delete=CASCADE, verbose_name='user',)
-	to= ForeignKey(Person, on_delete=CASCADE, verbose_name='recipient')
+    to= ForeignKey(Person, on_delete=CASCADE, verbose_name='recipient')
     timestamp = DateTimeField('timestamp', auto_now_add=True, editable=False)
     body = TextField('body')
-
     def notify_ws_clients(self):
         """
         Inform client there is a new message.
@@ -30,7 +24,6 @@ class MessageModel(Model):
         channel_layer = get_channel_layer()
         #async_to_sync(channel_layer.group_send)(sender.pk, notification)
         async_to_sync(channel_layer.group_send)(to.pk, notification)
-
     def save(self, *args, **kwargs):
         """
         Trims white spaces, saves the message and notifies the recipient via WS
