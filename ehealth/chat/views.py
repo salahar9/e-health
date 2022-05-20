993e6af53bf09c17f34812453db2e047ce6c4c06
salahar9 <salahar9@gmail.com>
2022-05-20 16:13:42 +0100

    fixed bug in chat

--- ehealth/chat/views.py
+++ ehealth/chat/views.py
@@ -3,8 +3,8 @@ from chat.models import Message
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.db.models import Q
-from django.db.models.aggregates import Count,F
-
+from django.db.models.aggregates import Count
+from django.db.models import F
 def chat(request):
 	chats=Message.objects.filter(Q(sender=request.user.person) | Q(to=request.user.person)).annotate(unread=Count("seen",filter=Q(seen=False)),holder=(request.user.person==sender))
 	return render(request,"chat/chats.html",{"chats":chats})
