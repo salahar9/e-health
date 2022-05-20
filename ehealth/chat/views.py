58f13ee963e0bd750d64a5a90c892cc64c2e3041
salahar9 <salahar9@gmail.com>
2022-05-20 16:40:59 +0100

    fixing all chats

--- ehealth/chat/views.py
+++ ehealth/chat/views.py
@@ -4,9 +4,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.db.models import Q
 from django.db.models.aggregates import Count
-from django.db.models import F
+from django.db.models import Case,When,Value
 def chat(request):
-	chats=Message.objects.filter(Q(sender=request.user.person) | Q(to=request.user.person)).annotate(unread=Count("seen",filter=Q(seen=False)),holder=F("sender")==request.user.person)
+	chats=Message.objects.filter(Q(sender=request.user.person) | Q(to=request.user.person)).annotate(unread=Count("seen",filter=Q(seen=False)),
+		holder=Case(
+			When(sender_pk__eq=request.user.person,then=1),
+
+			When(to_pk__eq=request.user.person,then=0)
+		))
 	return render(request,"chat/chats.html",{"chats":chats})
 @require_POST
 @csrf_exempt
