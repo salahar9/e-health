6e21b58d4d4d50b51827607cc8d3e40b14f7fab7
salahar9 <salahar9@gmail.com>
2022-05-22 19:58:35 +0100

    yarvi

--- ehealth/notifications/consumers.py
+++ ehealth/notifications/consumers.py
@@ -10,13 +10,11 @@ import channels.layers
 
 class VisiteConsumer(WebsocketConsumer):
     def connect(self):
-        
         async_to_sync(self.channel_layer.group_add)( self.scope["user"].person.doctor.INP, self.channel_name)
         self.accept()
 
     def disconnect(self,y):
                 async_to_sync(self.channel_layer.group_discard)(self.scope["user"].person.doctor.INP, self.channel_name)
-
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import consumers
 
 websocket_urlpatterns = [
     path(r'notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
-    path(r'notif/pharma/<int:pk>/', consumers.VisitePharmaConsumer),
+    url(r'notif/pharma/<int:pk>/', consumers.VisitePharmaConsumer.as_asgi()),
 ]
