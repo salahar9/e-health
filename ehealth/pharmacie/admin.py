410f56d85c923f7172b32c05b7e67a5d2843c552
salahar9 <salahar9@gmail.com>
2022-05-22 15:09:53 +0100

    fixing bugs in admin

--- ehealth/pharmacie/admin.py
+++ ehealth/pharmacie/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from pharmacie.models import Pharmacie
+from pharmacie.models import Pharmacie,Visite
 
 class PharmacieAdmin(admin.ModelAdmin):
     list_display = ("INP",'nom',"ville","activated")
@@ -7,4 +7,4 @@ class PharmacieVisiteAdmin(admin.ModelAdmin):
     list_display = ("pharma_id",'patient_id',"date_created")
     
 admin.site.register(Pharmacie, PharmacieAdmin) 
-admin.site.register(Pharmacie, PharmacieVisiteAdmin)
+admin.site.register(Visite, PharmacieVisiteAdmin)
