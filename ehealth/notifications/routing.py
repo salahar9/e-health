1120e40e94db830382c87bc1d60d2566135d5c65
salahar9 <salahar9@gmail.com>
2022-05-22 16:57:13 +0100

    url not path

--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path,url
 
 from . import consumers
 
 websocket_urlpatterns = [
     path(r'notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
 
-    path(r'^notif/pharma//(?P<room_name>[^/]+)/', consumers.VisitePharmaConsumer.as_asgi()),
+    url(r'^notif/pharma//(?P<room_name>[^/]+)/', consumers.VisitePharmaConsumer.as_asgi()),
 ]
