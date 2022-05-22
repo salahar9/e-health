ea1f573c06110c7ad581fdcac1b105b9333bcc9c
salahar9 <salahar9@gmail.com>
2022-05-22 20:05:23 +0100

    str int

--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path(r'notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
+    path(r'notif/<str:pk>/', consumers.VisiteConsumer.as_asgi()),
     path(r'notif/pharma/<int:pk>/', consumers.VisitePharmaConsumer.as_asgi()),
 ]
