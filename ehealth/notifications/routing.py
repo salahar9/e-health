e3d11c03c1ae2a7adb310cacac2cba45a127f6e3
salahar9 <salahar9@gmail.com>
2022-05-22 16:54:29 +0100

    check

--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path(r'notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
-    path(r'pharmavisite/<int:pk>/', consumers.VisitePharmaConsumer.as_asgi()),
-    path(r'^notif/pharma//(?P<room_name>[^/]+)/',consumers),
+
+    path(r'^notif/pharma//(?P<room_name>[^/]+)/', consumers.VisitePharmaConsumer.as_asgi()),
 ]
