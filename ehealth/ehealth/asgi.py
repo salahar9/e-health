daae77e44ac87fe00b1612046ce81be6a482dc72
salahar9 <salahar9@gmail.com>
2022-05-23 03:03:46 +0100

    router

--- ehealth/ehealth/asgi.py
+++ ehealth/ehealth/asgi.py
@@ -26,11 +26,15 @@ application = ProtocolTypeRouter({
         URLRouter(
             notifications.routing.websocket_urlpatterns,
             
-        ),
+        ),)
+    "websocket": AuthMiddlewareStack(
         URLRouter(
-        chat.routing.websocket_urlpatterns,
+            chat.routing.websocket_urlpatterns,
+            
+        ),
         )
-    ),
+
+    
     
     # Just HTTP for now. (We can add other protocols later.)
 })
