bde0a6fb220eda273c1ff54df8adc9c6a004e2e0
salahar9 <salahar9@gmail.com>
2022-05-23 03:43:08 +0100

    fixing notifs

--- ehealth/ehealth/asgi.py
+++ ehealth/ehealth/asgi.py
@@ -24,8 +24,7 @@ application = ProtocolTypeRouter({
     
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            [notifications.routing.websocket_urlpatterns,
-            chat.routing.websocket_urlpatterns,]
+            notifications.routing.websocket_urlpatterns+chat.routing.websocket_urlpatterns
             
         ),),
   
