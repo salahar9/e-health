bbf70662af7ef986026498c91c5e471b2480d797
salahar9 <salahar9@gmail.com>
2022-05-23 06:02:07 +0100

    test

--- ehealth/chat/views.py
+++ ehealth/chat/views.py
@@ -56,7 +56,7 @@ def fetch(request,pk):
 			When(sender_id=request.user.person,then=1),
 
 			When(to_id=request.user.person,then=0)
-		))[:15]
+		)).order_by("timestamp")
 	messages = list(messages.values())
 	return JsonResponse(messages,safe=False)
 	
