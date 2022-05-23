476a6d6b1aa78a291aeea7038bdcaaa7895ac042
salahar9 <salahar9@gmail.com>
2022-05-23 01:30:49 +0100

    tgawid dzb

--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'^notif/(?P<room_name>\w+)/$', consumers.VisiteConsumer.as_asgi()),
-    re_path(r'notif/pharma/(?P<room_name>\w+)/$', consumers.VisitePharmaConsumer.as_asgi()),
+   # re_path(r'notif/pharma/(?P<room_name>\w+)/$', consumers.VisitePharmaConsumer.as_asgi()),
 
 ]
