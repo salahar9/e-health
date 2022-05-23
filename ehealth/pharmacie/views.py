07ce97bad7d5179e1576b8e2bbe97920375c94ae
salahar9 <salahar9@gmail.com>
2022-05-23 12:21:42 +0100

    ml

--- ehealth/pharmacie/views.py
+++ ehealth/pharmacie/views.py
@@ -112,10 +112,10 @@ def mutuelle(request):
 		ordo.date_purchase=datetime.datetime.now()
 		ordo.save()
 		try:
-			mut=AllMutuelle(id_visite=ordo.visite_id,total=0,mutuelle_status="P")
+			mut=AllMutuelle(id_visite=ordo.id_visite,total=0,mutuelle_status="P")
 			mut.save()
 		except:
-			AllMutuelle.objects.get(id_visite=ordo.visite_id)
+			AllMutuelle.objects.get(id_visite=ordo.id_visite)
 		mut.tot+=ordo.price
 		mut.save()
 
