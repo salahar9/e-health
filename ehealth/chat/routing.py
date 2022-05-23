128f6c95fd52e5b94f55463d6b948b98c0d99d22
salahar9 <salahar9@gmail.com>
2022-05-23 01:52:16 +0100

    see

--- ehealth/chat/routing.py
+++ ehealth/chat/routing.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 websocket_urlpatterns = [
 
-	path(r'notif/chat/<int:pk>/', consumers.ChatConsumer.as_asgi()),
+	path(r'pharma/<int:pk>/', consumers.ChatConsumer.as_asgi()),
 
 ]
