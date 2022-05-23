aa2ce9b5d2f08218fac1cd3eef026cb8e031a773
salahar9 <salahar9@gmail.com>
2022-05-23 05:02:39 +0100

    k

--- ehealth/chat/consumers.py
+++ ehealth/chat/consumers.py
@@ -35,14 +35,14 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     def receive_msg(self,message):
-        text_data_json = json.loads(message)
-        message = text_data_json['message']
+        #text_data_json = json.loads(message)
+        #message = text_data_json['message']
 
         # Send message to room group
         self.send(text_data=json.dumps(
             {
 
-            'message': message,
+            'message': "message",
            
             
             }
