2a2777ba24f1d7cc1591aa7e4e6e920f42e9d19e
salahar9 <salahar9@gmail.com>
2022-05-23 01:40:18 +0100

    az

--- ehealth/notifications/routing.py
+++ ehealth/notifications/routing.py
@@ -3,7 +3,7 @@ from django.urls import path,re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path(r'notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
+    path('notif/<int:pk>/', consumers.VisiteConsumer.as_asgi()),
    # re_path(r'notif/pharma/(?P<room_name>\w+)/$', consumers.VisitePharmaConsumer.as_asgi()),
 
 ]
