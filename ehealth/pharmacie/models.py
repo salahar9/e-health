e4e798886f018940a11f95fbb78d7463eaa09601
salahar9 <salahar9@gmail.com>
2022-05-23 06:37:53 +0100

    fix

--- ehealth/pharmacie/models.py
+++ ehealth/pharmacie/models.py
@@ -31,7 +31,7 @@ def up(sender, instance,**kwargs):
             'type': 'send.visite',
 
             "visite":{			
-            			"visite":f"visites/{instance.pk}",
+            			"visite":f"/patient/visites/{instance.pk}",
                         "name":instance.patient_id.person_id.nom+" "+instance.patient_id.person_id.prenom,
                         "img":instance.patient_id.person_id.img.url,
                         "email":instance.patient_id.person_id.user.email,
